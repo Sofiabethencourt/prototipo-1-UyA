da3ce1f6872d7156d41ac179ea25273e50415713
Sofia Bethencourt <144826520+Sofiabethencourt@users.noreply.github.com>
2026-03-26 12:07:54 +0000

    Idiomas puesto

---
+++ taxi-app/js/languajes.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const lang = new URLSearchParams(window.location.search).get("lang") || "es";
+
+    // Cambiar textos
+    if (lang === "en") {
+        document.querySelectorAll("[data-en]").forEach(el => {
+            el.textContent = el.dataset.en;
+        });
+    }
+
+    document.querySelectorAll("[data-en]").forEach(el => {
+        if (el.tagName === "INPUT") {
+            el.placeholder = el.dataset.en;
+        } else {
+            el.textContent = el.dataset.en;
+        }
+    });
+
+    // Botones
+    const btnEs = document.getElementById("btn-es");
+    const btnEn = document.getElementById("btn-en");
+
+    if (lang === "en") {
+        btnEn.classList.add("btn-dark");
+        btnEn.classList.remove("btn-outline-dark");
+
+        btnEs.classList.add("btn-outline-dark");
+        btnEs.classList.remove("btn-dark");
+    } else {
+        btnEs.classList.add("btn-dark");
+        btnEs.classList.remove("btn-outline-dark");
+
+        btnEn.classList.add("btn-outline-dark");
+        btnEn.classList.remove("btn-dark");
+    }
+});
