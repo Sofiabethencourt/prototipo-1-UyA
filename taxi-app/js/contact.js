29a72134e6b3a3c85b5d868acd4e4ef00121ccf9
Sofia Bethencourt <144826520+Sofiabethencourt@users.noreply.github.com>
2026-03-30 10:34:59 +0000

    Cambio enlace de gmail en contacto

--- taxi-app/js/contact.js
+++ taxi-app/js/contact.js
@@ -1,34 +1,34 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('contactForm');
+// document.addEventListener('DOMContentLoaded', () => {
+//     const form = document.getElementById('contactForm');
 
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
+//     form.addEventListener('submit', (event) => {
+//         event.preventDefault();
 
-        if (!form.checkValidity()) {
-            form.classList.add('was-validated');
-        } else {
-            enviarExito(form);
-        }
-    });
-});
+//         if (!form.checkValidity()) {
+//             form.classList.add('was-validated');
+//         } else {
+//             enviarExito(form);
+//         }
+//     });
+// });
 
-function enviarExito(form) {
-    const esIngles = form.querySelector('h1').innerText.includes('Us');
-    const mensaje = esIngles ? "Message sent successfully!" : "¡Mensaje enviado con éxito!";
+// function enviarExito(form) {
+//     const esIngles = form.querySelector('h1').innerText.includes('Us');
+//     const mensaje = esIngles ? "Message sent successfully!" : "¡Mensaje enviado con éxito!";
 
-    const successDiv = document.getElementById('successMessage');
-    const successText = document.getElementById('successText');
+//     const successDiv = document.getElementById('successMessage');
+//     const successText = document.getElementById('successText');
 
-    // mostrar mensaje de éxito
-    successText.textContent = mensaje;
-    successDiv.classList.remove('d-none');
+//     // mostrar mensaje de éxito
+//     successText.textContent = mensaje;
+//     successDiv.classList.remove('d-none');
 
-    // sube la pantalla para mostrar el mensaje de éxito
-    window.scrollTo({
-        top: 0, 
-        behavior: 'smooth'
-    });
+//     // sube la pantalla para mostrar el mensaje de éxito
+//     window.scrollTo({
+//         top: 0, 
+//         behavior: 'smooth'
+//     });
     
-    form.reset(); // vaciar
-    form.classList.remove('was-validated');
-}
+//     form.reset(); // vaciar
+//     form.classList.remove('was-validated');
+// }
